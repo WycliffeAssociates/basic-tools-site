55504d78a07839e75a7a00e186b24807dc71fb53
Dan <dan@example.com>
2024-01-08 14:04:45 -0500

    fix output

--- functions/fetch-releases-timer/__init__.py
+++ functions/fetch-releases-timer/__init__.py
@@ -2,7 +2,7 @@ import logging
 import datetime
 import azure.functions as func
 
-def main(updatetimer: func.TimerRequest) -> None:
+def main(updatetimer: func.TimerRequest, msg: func.Out[str]) -> None:
     utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
     """Main script to run the function"""
     logging.info('Sending queue message to start fetching releases')
