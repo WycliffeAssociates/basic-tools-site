8011a81dd69fb47afd1e416cda715886ca048c74
Dan <dan@example.com>
2024-01-08 14:50:59 -0500

    fix output, add logging

--- functions/fetch-releases-timer/__init__.py
+++ functions/fetch-releases-timer/__init__.py
@@ -5,8 +5,8 @@ import azure.functions as func
 def main(updatetimer: func.TimerRequest, msg: func.Out[str]) -> None:
     utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
     """Main script to run the function"""
-    logging.info('Sending queue message to start fetching releases')
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
     msg.set("fetch-releases-queue")
 
-    return 'OK'
+    logging.info('msg entered in queue')
